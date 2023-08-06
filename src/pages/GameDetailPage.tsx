456429c13e3f3324cfb8834f59108705c1bd0dff
Jason Weng <jasonweng95@gmail.com>
2023-08-07 00:59:53 +1000

    handle page error and fetch game details

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,11 +1,10 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import { Heading, Spinner, Text } from "@chakra-ui/react";
 
-
 const GameDetailPage = () => {
-  const { slug} = useParams();
-  const { data: game, isLoading, error} = useGame(slug!);
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
 
   if (isLoading) return <Spinner />;
 
@@ -13,10 +12,10 @@ const GameDetailPage = () => {
 
   return (
     <>
-     <Heading>{game.name}</Heading>
-     <Text>{game.description_raw}</Text>
+      <Heading>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
     </>
-  )
-}
+  );
+};
 
-export default GameDetailPage
+export default GameDetailPage;
