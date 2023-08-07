216e9821d7b2b68827d63a7743ac3b3a86638d44
Jason Weng <jasonweng95@gmail.com>
2023-08-08 00:40:58 +1000

    Build game attributes

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,23 +1,11 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 import ms from 'ms';
-import { Platform } from './usePlatforms';
 import APIClient, { FetchResponse } from '../services/api-client';
 import useGameQueryStore from '../store';
+import { Game } from '../entites/Game';
 
 const apiClient = new APIClient<Game>('/games');
 
-export interface Game {
-    id: number;
-    name: string;
-    slug: string;
-    description_raw: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
-    rating_top: number;
-    rating: number;
-  }
-  
 // params: query string parameter to the data hook
 const useGames = () =>  {
   const gameQuery = useGameQueryStore(s => s.gameQuery);
