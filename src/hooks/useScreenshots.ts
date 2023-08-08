25dd134311c9595d62d417df61f5701ba0e768a6
Jason Weng <jasonweng95@gmail.com>
2023-08-08 23:56:16 +1000

    Build game screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { Screenshots } from "../entites/Screenshots"
+import APIClient from "../services/api-client"
+
+
+const useScreenshots = (gameId: number) => {
+    
+    const apiClinet = new APIClient<Screenshots>(`/games/${gameId}/screenshots`);
+
+    return useQuery({
+        queryKey: ['screenshots', gameId],
+        queryFn: apiClinet.getAll,
+    })
+}
+
+export default useScreenshots;
